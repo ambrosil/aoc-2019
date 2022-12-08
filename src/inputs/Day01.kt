59703d8c392a1d3e656de797e94964806c80586d
Luca Ambrosi <luca@luca.it>
2022-12-08 15:28:38 +0100

    Day01

---
+++ src/inputs/Day01.kt
@@ -0,0 +1,28 @@
+package inputs
+
+import readInput
+import kotlin.math.floor
+
+fun main() {
+
+    fun part1(input: List<String>) = input.sumOf {
+        (floor(it.toDouble() / 3) - 2).toInt()
+    }
+
+    fun part2(input: List<String>) = input.sumOf { it.requiredFuel() }
+
+    val input = readInput("inputs/Day01")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun String.requiredFuel() = calcFuel(toInt())
+
+fun calcFuel(required: Int): Int {
+    val value = (floor(required / 3.0) - 2).toInt()
+    return if (value < 0) {
+        0
+    } else {
+        value + calcFuel(value)
+    }
+}
